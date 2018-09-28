8920646e9de772eef1656d667d7364a7e9fb8f39
valeria campilongo <cvaleriac@hotmail.it>
2018-09-28 20:25:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,17 @@ class Application
         resp.write "Your cart is empty!"
       else @@cart.each do |item|
           resp.write "#{item}\n"
+        elsif req.path.match(/add/)
+    search_term = req.params["item"]
+    if @@items.include?(search_term)
+      @@cart << search_term
+      resp.write "added #{search_term}"
+    else
+      resp.write "We don't have that item"
+    end
+  else
+    resp.write "Path Not Found"
+  end
     end
   end
 end
